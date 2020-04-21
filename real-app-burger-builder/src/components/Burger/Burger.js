47268f791ab6f824a9ed348e0c787d339b347e28
pcontra <pedro.rc.andrade@outlook.pt>
2020-04-22 00:30:58 +0100

    Finished till 130 video

--- real-app-burger-builder/src/components/Burger/Burger.js
+++ real-app-burger-builder/src/components/Burger/Burger.js
@@ -3,11 +3,24 @@ import BurgerIngredient from "../BurgerIngredient/BurgerIngredient";
 import "./Burger.css";
 
 const Burger = (props) => {
+  let ingredients = Object.keys(props.ingredients)
+    .map((key) => {
+      return [...Array(props.ingredients[key])].map((_, index) => {
+        return <BurgerIngredient key={key + index} type={key} />;
+      });
+    })
+    .reduce((acc, item) => {
+      return acc.concat(item);
+    }, []);
+
+  if (ingredients.length === 0) {
+    ingredients = <p>Please start adding ingredients!</p>;
+  }
+
   return (
     <div className="Burger">
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {ingredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
--- real-app-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ real-app-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,10 +2,24 @@ import React, { Component } from "react";
 import Burger from "../../components/Burger/Burger";
 
 class BuilderBurger extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {}
+  // }
+
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0,
+    },
+  };
+
   render() {
     return (
       <>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>Build Controls</div>
       </>
     );
