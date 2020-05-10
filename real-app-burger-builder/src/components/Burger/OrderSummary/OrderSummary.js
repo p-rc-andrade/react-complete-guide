de699ad7384fb9d58159cd3a7cf67d45964313a8
pcontra <pedro.rc.andrade@outlook.pt>
2020-05-10 16:24:35 +0100

    Finished till lesson 160 - Burger Builder basic - done

--- real-app-burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ real-app-burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,32 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
 import Button from "../../UI/Button/Button";
-const OrderSummary = (props) => {
-  const ingredientsSummary = Object.keys(props.ingredients).map((ingKey) => {
-    return (
-      <li key={ingKey}>
-        <span style={{ textTransform: "capitalize" }}>{ingKey}</span>:{" "}
-        {props.ingredients[ingKey]}
-      </li>
-    );
-  });
 
-  return (
-    <>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>{ingredientsSummary}</ul>
-      <p>
-        <strong>Total Price:</strong> {props.price.toFixed(2)}
-      </p>
-      <p>Continue to Checkout ?</p>
-      <Button btnType="Danger" clicked={props.purchaseCancelHandler}>
-        CANCEL
-      </Button>
-      <Button btnType="Success" clicked={() => props.purchaseContinueHandler()}>
-        CONTINUE
-      </Button>
-    </>
-  );
-};
+class OrderSummary extends Component {
+  componentDidUpdate() {
+    console.log("[OrderSummary] Will Update");
+  }
+
+  render() {
+    const ingredientsSummary = Object.keys(this.props.ingredients).map(
+      (ingKey) => {
+        return (
+          <li key={ingKey}>
+            <span style={{ textTransform: "capitalize" }}>{ingKey}</span>:{" "}
+            {this.props.ingredients[ingKey]}
+          </li>
+        );
+      }
+    );
+
+    return (
+      <>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>{ingredientsSummary}</ul>
+        <p>
+          <strong>Total Price:</strong> {this.props.price.toFixed(2)}
+        </p>
+        <p>Continue to Checkout ?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelHandler}>
+          CANCEL
+        </Button>
+        <Button
+          btnType="Success"
+          clicked={() => this.props.purchaseContinueHandler()}
+        >
+          CONTINUE
+        </Button>
+      </>
+    );
+  }
+}
 
 export default OrderSummary;
--- real-app-burger-builder/src/hoc/Layout/Layout.js
+++ real-app-burger-builder/src/hoc/Layout/Layout.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import Toolbar from "../Navigation/Toolbar/Toolbar";
-import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
+import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
+import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
 
 import "./Layout.css";
 
 class Layout extends Component {
   state = {
-    showSideDrawer: true,
+    showSideDrawer: false,
   };
 
   sideDrawerToggleHandler = () => {
